b85bd9313cd5f618e772427fbea1957f6881746f
Najmus Sakib <nazmusakib412@gmail.com>
2023-03-15 00:35:36 +0600

    Add antd

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import MainLayout from '@/components/Layout/Layout'
 
 export const metadata = {
   title: 'Create Next App',
@@ -12,7 +13,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <MainLayout>
+          {children}
+        </MainLayout>
+      </body>
     </html>
   )
 }
