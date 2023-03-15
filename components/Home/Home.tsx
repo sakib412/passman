ca00079ac83491a8ea89ece8016ecbd85c204d6c
Najmus Sakib <nazmusakib412@gmail.com>
2023-03-15 21:54:45 +0600

    add folder

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,57 +1,64 @@
 "use client"
-import React from 'react'
-import { MenuProps, Menu, Layout, theme, Space, Tag, Table, Dropdown } from 'antd';
+import React, { useState } from 'react'
+import { MenuProps, Menu, Layout, theme, Space, Tag, Table, Dropdown, Button } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
-import { DownOutlined, LaptopOutlined, MenuOutlined, MoreOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import { CopyOutlined, DeleteOutlined, FolderFilled, MoreOutlined, RightCircleOutlined } from '@ant-design/icons';
 
 const { Sider } = Layout;
 
-const sideMenu: MenuProps['items'] = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
-    (icon, index) => {
-        const key = String(index + 1);
-
-        return {
-            key: `sub${key}`,
-            icon: React.createElement(icon),
-            label: `subnav ${key}`,
-
-            children: new Array(4).fill(null).map((_, j) => {
-                const subKey = index * 4 + j + 1;
-                return {
-                    key: subKey,
-                    label: `option${subKey}`,
-                };
-            }),
-        };
-    },
-);
+interface Folder {
+    id: number;
+    name: string;
+}
 
 
-const items: MenuProps['items'] = [
+const foldersData: Folder[] = [
+    { id: 1, name: 'Folder 1' },
+    { id: 2, name: 'Folder 2' },
+    { id: 3, name: 'Folder 3' },
+]
+
+
+
+
+const itemMenu: MenuProps['items'] = [
     {
-        label: "1st menu item",
+        label: "Copy username",
         key: '0',
+        icon: <CopyOutlined />
+
     },
     {
-        label: "Hi",
+        label: "Copy password",
         key: '1',
+        icon: <CopyOutlined />
     },
     {
-        type: 'divider',
+        label: "Move to another folder",
+        key: '2',
+        icon: <RightCircleOutlined />
+
     },
     {
-        label: '3rd menu item',
+        label: "Delete",
         key: '3',
+        icon: <DeleteOutlined />,
+        danger: true
     },
+
+
 ];
 
 
 interface DataType {
+    id: number | string;
     key: string;
     name: string;
-    age: number;
-    address: string;
-    tags: string[];
+    username: string;
+    password: string;
+    url?: string;
+    notes?: string;
+    owner: number;
 }
 
 const columns: ColumnsType<DataType> = [
@@ -59,43 +66,22 @@ const columns: ColumnsType<DataType> = [
         title: 'Name',
         dataIndex: 'name',
         key: 'name',
-        render: (text) => <a>{text}</a>,
+        render: (_, { username, name }) => <>{username}<br />{name} </>,
     },
+
     {
-        title: 'Age',
-        dataIndex: 'age',
-        key: 'age',
+        title: 'Owner',
+        dataIndex: 'owner',
+        key: 'owner',
+        render: (owner: number) => <Tag color={owner == 1 ? "green" : "red"}>{owner == 1 ? "Me" : "Other"}</Tag>
     },
-    {
-        title: 'Address',
-        dataIndex: 'address',
-        key: 'address',
-    },
-    {
-        title: 'Tags',
-        key: 'tags',
-        dataIndex: 'tags',
-        render: (_, { tags }) => (
-            <>
-                {tags.map((tag) => {
-                    let color = tag.length > 5 ? 'geekblue' : 'green';
-                    if (tag === 'loser') {
-                        color = 'volcano';
-                    }
-                    return (
-                        <Tag color={color} key={tag}>
-                            {tag.toUpperCase()}
-                        </Tag>
-                    );
-                })}
-            </>
-        ),
-    },
+
     {
         title: 'Action',
         key: 'action',
+        align: 'right',
         render: (_, record) => (
-            <Dropdown menu={{ items, onClick: (item) => { console.log(item, record) } }} trigger={['click']}>
+            <Dropdown menu={{ items: itemMenu, onClick: (item) => { console.log(item, record) } }} trigger={['click']}>
                 <Space style={{ cursor: 'pointer' }}>
                     <MoreOutlined style={{ fontSize: '23px' }} />
                 </Space>
@@ -106,32 +92,94 @@ const columns: ColumnsType<DataType> = [
 
 const data: DataType[] = [
     {
+        id: '1',
         key: '1',
         name: 'John Brown',
-        age: 32,
-        address: 'New York No. 1 Lake Park',
-        tags: ['nice', 'developer'],
+        username: 'masd@sdgsd.sdfsf',
+        password: '12312321',
+        owner: 2,
+        notes: 'New York No. 1 Lake Park',
+        url: 'https://www.google.com',
     },
     {
+        id: '2',
         key: '2',
         name: 'Jim Green',
-        age: 42,
-        address: 'London No. 1 Lake Park',
-        tags: ['loser'],
+        username: 'new',
+        password: '12312321',
+        owner: 1,
+        notes: 'London No. 1 Lake Park',
+        url: 'https://www.google.com',
     },
     {
+        id: '3',
         key: '3',
         name: 'Joe Black',
-        age: 32,
-        address: 'Sydney No. 1 Lake Park',
-        tags: ['cool', 'teacher'],
-    },
+        username: 'nameW@amsdi.cas',
+        password: '12312321',
+        owner: 1,
+        notes: 'Sidney No. 1 Lake Park',
+        url: 'https://www.google.com',
+    }
+
 ];
 
+
+
 const Home = () => {
+    const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([])
+    const [folders, setFolders] = useState<Folder[]>(() => foldersData)
+
+    const [loading, setLoading] = useState(false);
+
+    const addFolder = () => {
+        setFolders(f => [...f, { id: f.length + 1, name: `Folder ${f.length + 1}` }])
+    }
+
+    const start = () => {
+        setLoading(true);
+        // ajax request after empty completing
+        setTimeout(() => {
+            setSelectedRowKeys([]);
+            setLoading(false);
+        }, 1000);
+    };
+    const sideMenu: MenuProps['items'] = [FolderFilled].map(
+        (icon, index) => {
+            const key = String(index + 1);
+
+            return {
+                key: `folder${key}`,
+                type: 'group',
+                icon: React.createElement(icon),
+                label: (<div className='d-flex justify-content-between text-white fs-5'>
+                    <span>Folder</span>
+                    <span style={{ cursor: 'pointer' }}
+                        onClick={addFolder}>+</span>
+                </div>),
+
+                children: folders.map(({ id, name }, j) => {
+                    return {
+                        key: id,
+                        label: name,
+                    };
+                }),
+            };
+        },
+    );
+    const hasSelected = selectedRowKeys.length > 0;
+    const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
+        console.log('selectedRowKeys changed: ', newSelectedRowKeys);
+        setSelectedRowKeys(newSelectedRowKeys);
+    };
     const {
         token: { colorBgContainer },
     } = theme.useToken();
+
+    const rowSelection = {
+        selectedRowKeys,
+        onChange: onSelectChange,
+    };
 
     return (
         <div className='d-flex'>
@@ -139,15 +187,26 @@ const Home = () => {
                 <Menu
                     mode="inline"
                     defaultSelectedKeys={['1']}
-                    defaultOpenKeys={['sub1']}
+                    defaultOpenKeys={['folder1']}
+                    theme="dark"
                     style={{ height: '100%', borderRight: 0 }}
                     items={sideMenu}
                 />
             </Sider>
-            <div className='px-3'>
-                Hello
-                <Table columns={columns} dataSource={data} />
+            <div className='px-3 w-100'>
+                <div className="d-flex justify-content-between mb-3">
+                    <h4>
+                        All vaults
+                    </h4>
+                    <div>
+                        <Button type="primary" onClick={start} danger disabled={!hasSelected} loading={loading}>
+                            Delete
+                        </Button>
+                        <Button type="primary" className='ms-2'>New item +</Button>
+                    </div>
+                </div>
 
+                <Table size='large' rowSelection={rowSelection} columns={columns} dataSource={data} />
             </div>
         </div>
     )
