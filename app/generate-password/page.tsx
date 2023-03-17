d0ad007223b93772b67e07ba17256afc9abf13a9
Najmus Sakib <nazmusakib412@gmail.com>
2023-03-17 19:55:47 +0600

    Add: metadata for password generator

--- app/generate-password/page.tsx
+++ app/generate-password/page.tsx
@@ -1,5 +1,10 @@
 import GeneratePassword from "@/components/GeneratePassword/GeneratePassword";
 
+export const metadata = {
+    title: 'Generate Password | Password manager',
+    description: 'Generate a strong password',
+}
+
 export default function GeneratePasswordPage() {
     return (
         <GeneratePassword />
